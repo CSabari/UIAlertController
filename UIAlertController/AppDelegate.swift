2387ddb56e0bac076f48e5f29b7831e9fc140488
Manu <Sabari.mano@gmail.com>
2018-02-10 13:11:32 +0530

    Update AppDelegate.swift

--- UIAlertController/AppDelegate.swift
+++ UIAlertController/AppDelegate.swift
@@ -2,7 +2,7 @@
 //  AppDelegate.swift
 //  UIAlertController
 //
-//  Created by TRESELLE on 10/02/18.
+//  Created by SABA on 10/02/18.
 //  Copyright © 2018 com.ios.sample. All rights reserved.
 //
 
